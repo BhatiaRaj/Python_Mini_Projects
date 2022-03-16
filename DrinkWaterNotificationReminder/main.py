7005c2933734659d16abfcadb8e7ad10c039df84
BhatiaRaj <97431540+BhatiaRaj@users.noreply.github.com>
2022-03-16 12:24:56 +0530

    Drink Water Notification Reminder
    It is desktop notification.
    It will reminder you for drinking water.

---
+++ DrinkWaterNotificationReminder/main.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if __name__=='__main__':
+    while True:
+        notification.notify(
+            title="** Please Drink Water Now !!",
+            message="Drinking Water Helps to Maintain the Balance of Body Fluids.",
+            app_icon="R:\PythonProject\DrinkWaterNotificationReminder\Glass.ico",
+            timeout=10
+        )
+        time.sleep(60*60)
